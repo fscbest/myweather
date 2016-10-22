273282180e8ac913c3edc42d74c5341ad275f402
fscbest <romangeyber@gmail.com>
2016-10-22 22:19:08 +0300

    Fetch data through the service

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -6,8 +6,14 @@ import 'rxjs/Rx';
 @Injectable()
 export class WeatherService{
 
+  apiKey = 'b47f900fb57d3328';
+  conditionsUrl = 'http://api.wunderground.com/api/' + this.apiKey + '/conditions/q';
   constructor(private _http:Http){
     console.log("Service connected");
   }
 
+  getWeather(city, state){
+    return this._http.get(this.conditionsUrl+'/'+state+'/'+city+'.json')
+      .map(res => res.json());
+  }
 }
--- src/pages/weather/weather.ts
+++ src/pages/weather/weather.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 import {WeatherService} from "../../app/services/weather.service";
@@ -9,10 +9,18 @@ import {WeatherService} from "../../app/services/weather.service";
   providers: [WeatherService]
 })
 export class WeatherPage {
+  city = "Boston";
+  state = "MA";
 
   constructor(public navCtrl: NavController,
               private _weatherService: WeatherService) {
 
   }
 
+  ngOnInit(){
+     this._weatherService.getWeather(this.city, this.state)
+       .subscribe(weather => {
+         console.log(weather);
+       });
+  }
 }
